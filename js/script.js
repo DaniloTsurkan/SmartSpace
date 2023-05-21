ef2aedbb6eb86c69b01292af454032d0aaf18bc0
DaniloTsurkan <126474745+DaniloTsurkan@users.noreply.github.com>
2023-05-21 07:31:59 +0300

    Pre relize website

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ const swiper = new Swiper('.slider-main-block', {
     prevEl: '.body-main-block__arrow.swiper-button-prev',
   },
 });
+
+const tabNavItems =document.querySelectorAll('.tabs-deals__button');
+const tabItems =document.querySelectorAll('.item-tabs');
+
+document.addEventListener("click", function (e) {
+	const targetElement=e.target;
+	if(targetElement.closest('.tabs-deals__button')){
+	}
+});
